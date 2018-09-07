bf26e6fbd1e14b211c968ee82f86ea51dfdc4130
unknown <luzexi@snda.com>
2018-09-07 22:45:42 +0800

    fix bat

--- makeTable.bat
+++ makeTable.bat
@@ -18,18 +18,18 @@ call exportText.bat
 cd ..
 
 
-:: @echo off
-:: setlocal enabledelayedexpansion
-:: set fn=.\tables\output\TextDefine.cs
-:: set n=3
-:: (for /f "tokens=*" %%i in ('type "%fn%"') do (
-:: set /a m+=1
-:: if !m!==%n% (echo public class LText{) else echo %%i))>TextDefine.cs
-:: move /y TextDefine.cs "%fn%"
-:: @echo on
+@echo off
+setlocal enabledelayedexpansion
+set fn=.\tables\output\TextDefine.cs
+set n=3
+(for /f "tokens=*" %%i in ('type "%fn%"') do (
+set /a m+=1
+if !m!==%n% (echo public class TText{) else echo %%i))>TextDefine.cs
+move /y TextDefine.cs "%fn%"
+@echo on
 
-:: copy .\tables\output\*.cs .\project\Assets\scripts\dataTable\ /y
-:: copy .\tables\output\*.bytes .\project\Assets\Resources\dat\ /y
+copy .\tables\output\*.cs .\project\Assets\scripts\dataTable\ /y
+copy .\tables\output\*.bytes .\project\Assets\Resources\dat\ /y
 
 :: .\tools\ClientDataTableHashExporter.exe .\project\Assets\Resources\dat\
 .\tools\ClientDataTableHashExporter.exe .\tables\output\
